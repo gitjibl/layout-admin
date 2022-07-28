bfbfc882d214baa4adb634b08ee9542cd5384afa
jibl-gitee <871048617@qq.com>
2022-07-28 17:35:32 +0800

    ok

--- src/layout/Menu.vue
+++ src/layout/Menu.vue
@@ -1,88 +1,31 @@
 <template>
-  <el-menu
-    style="width: 100%"
-    class="el-menu-demo"
-    :default-active="activeMenu"
-    background-color="#095a91"
-    text-color="#fff"
-    active-text-color="rgb(21 209 113)"
-    mode="horizontal"
-    unique-opened
-  >
-    <!--手动定义菜单-->
-    <!-- <template v-for="item in menuItems">
-      <template v-if="item.subs">
-        <el-submenu :index="item.index" :key="item.index">
-          <template slot="title">
-            <i :class="item.icon"></i>
-            <span slot="title">{{ item.title }}</span>
-          </template>
-          <template v-for="subItem in item.subs">
-            <el-submenu v-if="subItem.subs" :index="subItem.index" :key="subItem.index">
-              <template slot="title">{{ subItem.title }}</template>
-              <el-menu-item v-for="(threeItem,i) in subItem.subs" :key="i" :index="threeItem.index">
-                {{ threeItem.title }}</el-menu-item>
-            </el-submenu>
-            <el-menu-item v-else :index="subItem.index" :key="subItem.index">{{ subItem.title }}</el-menu-item>
-          </template>
-        </el-submenu>
-      </template>
-      <template v-else>
-
-        <el-menu-item :index="item.index" :key="item.index">
-          <i :class="item.icon"></i>
-          <span slot="title">{{ item.title }}</span>
-        </el-menu-item>
-      </template>
-    </template> -->
+  <el-menu style="width: 100%" class="el-menu-demo" :default-active="activeMenu" background-color="#095a91"
+    text-color="#fff" active-text-color="rgb(21 209 113)" mode="horizontal" unique-opened>
     <!--动态路由读取菜单 -->
     <!--1级菜单-->
     <template v-for="(route, index) in permission_routes">
       <template v-if="!route.hidden">
-        <el-menu-item
-          v-if="route.children && route.children.length == 1 && !route.alwaysShow"
-          :key="index"
-          :index="resolvePath(route.path, route.children[0].path)"
-        >
-          <app-link
-            :to="resolvePath(route.path, route.children[0].path)"
-            style="display: block"
-          >
-            <template
-              v-if="route.children[0].meta && !route.children[0].meta.imgUrl"
-            >
-              <item
-                v-if="route.children[0].meta"
-                :icon="route.children[0].meta && route.children[0].meta.icon"
-                :title="route.children[0].meta.title"
-              />
+        <el-menu-item v-if="route.children && route.children.length == 1 && !route.alwaysShow" :key="index"
+          :index="resolvePath(route.path, route.children[0].path)">
+          <app-link :to="resolvePath(route.path, route.children[0].path)" style="display: block">
+            <template v-if="route.children[0].meta && !route.children[0].meta.imgUrl">
+              <item v-if="route.children[0].meta" :icon="route.children[0].meta && route.children[0].meta.icon"
+                :title="route.children[0].meta.title" />
             </template>
             <template v-else>
-              <el-image
-                :src="route.children[0].meta.imgUrl"
-                style="height: 60px; width: 60px"
-              ></el-image>
+              <el-image :src="route.children[0].meta.imgUrl" style="height: 60px; width: 60px"></el-image>
             </template>
           </app-link>
         </el-menu-item>
 
         <el-submenu :index="route.path" :key="index" v-else>
           <template v-if="route.meta && !route.meta.imgUrl" slot="title">
-            <i :class="route.meta && route.meta.icon"></i>
-            <span slot="title">{{ route.meta.title }}</span>
+            <item v-if="route.meta" :icon="route.meta && route.meta.icon" :title="route.meta.title" />
           </template>
           <template v-else slot="title">
-            <el-image
-              :src="route.meta.imgUrl"
-              style="height: 60px; width: 60px"
-            ></el-image>
+            <el-image :src="route.meta.imgUrl" style="height: 60px; width: 60px"></el-image>
           </template>
-          <MenuItemGroup
-            v-for="(e, i) in route.children"
-            :key="i"
-            :item="e"
-            :base-path="route.path"
-          >
+          <MenuItemGroup v-for="(e, i) in route.children" :key="i" :item="e" :base-path="route.path">
           </MenuItemGroup>
         </el-submenu>
       </template>
@@ -92,115 +35,81 @@
 
 
 <script>
-import MenuItemGroup from "./MenuItemGroup";
-import Item from "./Item";
-import AppLink from "./Link";
-import path from "path";
-import { isExternal } from "@/utils/validate";
-import { mapGetters } from 'vuex'
-export default {
-  data() {
-    // this.onlyOneChild = null
-    return {
-      // onlyOneChild: null
-      // menuItems: [{
-      //     icon: "el-icon-s-home",
-      //     index: "dashboard",
-      //     title: "首页"
-      //   },
-      //   {
-      //     icon: "el-icon-question",
-      //     index: "baseform",
-      //     title: "测试页面",
-      //     subs: [{
-      //         index: "baseform",
-      //         title: "表单"
-      //       },
-      //       {
-      //         index: "2-1",
-      //         title: "图片",
-      //         subs: [{
-      //           index: "showimg",
-      //           title: "头像"
-      //         }]
-      //       }
-      //     ]
-      //   },
-      //   {
-      //     icon: "el-icon-info",
-      //     index: "error",
-      //     title: "错误页面",
-      //     subs: [{
-      //         index: "/error/401",
-      //         title: "401"
-      //       },
-      //       {
-      //         index: "/error/404",
-      //         title: "404"
-      //       }
-      //     ]
-      //   },
-      // ]
-    };
-  },
-  mounted() {
-    // console.log("routes列表", this.routes, this.$router, this.$route)
-  },
-  components: {
-    AppLink,
-    MenuItemGroup,
-    Item,
-  },
-  computed: {
-    // routes() {
-    //   // return this.$store.state.permission.routes;
-    //   return this.$router.options.routes;
-    // },
-    ...mapGetters(["permission_routes"]),
-    activeMenu() {
-      const route = this.$route;
-      const { meta, path } = route;
-      if (meta.activeMenu) {
-        return meta.activeMenu;
-      }
-      return path;
+  import MenuItemGroup from "./MenuItemGroup";
+  import Item from "./Item";
+  import AppLink from "./Link";
+  import path from "path";
+  import {
+    isExternal
+  } from "@/utils/validate";
+  import {
+    mapGetters
+  } from "vuex";
+  export default {
+    data() {
+      return {};
     },
-  },
-  methods: {
-    resolvePath(basePath, routePath) {
-      if (isExternal(routePath)) {
-        return routePath;
-      }
-      if (isExternal(basePath)) {
-        return basePath;
-      }
-      return path.resolve(basePath, routePath);
+    components: {
+      AppLink,
+      MenuItemGroup,
+      Item,
     },
-  },
-};
+    computed: {
+      // routes() {
+      //   // return this.$store.state.permission.routes;
+      //   return this.$router.options.routes;
+      // },
+      ...mapGetters(["permission_routes"]),
+      activeMenu() {
+        const route = this.$route;
+        const {
+          meta,
+          path
+        } = route;
+        if (meta.activeMenu) {
+          return meta.activeMenu;
+        }
+        return path;
+      },
+    },
+    methods: {
+      resolvePath(basePath, routePath) {
+        if (isExternal(routePath)) {
+          return routePath;
+        }
+        if (isExternal(basePath)) {
+          return basePath;
+        }
+        return path.resolve(basePath, routePath);
+      },
+    },
+  };
 </script>
 <style scoped>
-.el-menu-item i {
-  color: #eeeff3;
-}
+  .el-menu-item i {
+    color: #eeeff3;
+  }
 
-::v-deep .el-submenu__title i {
-  color: #eeeff3;
-}
+  ::v-deep .el-submenu__title i {
+    color: #eeeff3;
+  }
 
-::v-deep .el-submenu__title {
-  padding: 0 10px;
-}
+  ::v-deep .el-submenu__title {
+    padding: 0 6px;
+  }
 
-.el-menu.el-menu--horizontal {
-  border-bottom: solid 0px #e6e6e6;
-}
+  .el-menu.el-menu--horizontal {
+    border-bottom: solid 0px #e6e6e6;
+  }
 
-.el-menu-item {
-  padding: 0 10px;
-}
+  .el-menu-item {
+    padding: 0 6px;
+  }
 
-/* ::v-deep .el-icon-arrow-down:before {
+  /**
+  隐藏menu 下拉显示
+*/
+  ::v-deep .el-icon-arrow-down:before {
     content: "";
-  } */
+  }
 </style>
